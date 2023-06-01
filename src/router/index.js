1c206dc207a3ccf5a7b1a4b332856f1f7f36510f
Kaveesha <kalshandhanasekara@gmail.com>
2023-06-01 18:16:59 +0530

    update routers

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,50 @@ const routes = [
       },
     ],
   },
+  {
+    path: '/auth',
+    component:()=> import('@/layouts/AuthLayout.vue'),
+    children: [
+      {
+        path: '',
+        component:()=> import('@/views/Login.vue')
+      },
+      {
+        path: 'register',
+        component:()=> import('@/views/Registration.vue')
+      },
+      {
+        path: 'reset',
+        component:()=> import('@/views/ResetPass.vue')
+      },
+    ]
+  },
+  {
+    path: '/student',
+    component:()=> import('@/layouts/StudentLayout.vue'),
+    children: [
+      {
+        path: '',
+        component:()=> import('@/views/student/Dashboard.vue'),
+      },
+      {
+        path: 'dashboard',
+        component:()=> import('@/views/student/Dashboard.vue'),
+      },
+      {
+        path: 'messages',
+        component:()=> import('@/views/student/Messages.vue'),
+      },
+      {
+        path: 'reports',
+        component:()=> import('@/views/student/Reports.vue'),
+      },
+      {
+        path: 'classes',
+        component:()=> import('@/views/student/Classes.vue'),
+      },
+    ]
+  }
 ]
 
 const router = createRouter({
